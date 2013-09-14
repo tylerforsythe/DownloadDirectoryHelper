c6ad99c8109f9cd5771a832761d5ca5a0b13e9a4
Tyler Forsythe <forsythe.tyler@gmail.com>
2013-09-14 00:09:26 -0600

    Very first commit of project. Does nothing yet.

---
+++ DDH_UI/DirectoryTree.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+
+namespace DDH_UI
+{
+
+    public class DirectoryTree : TreeView
+    {
+        public event DirectorySelectedEventHandler DirectorySelected;
+        public delegate void DirectorySelectedEventHandler(object sender, DirectorySelectedEventArgs e);
+        private char _Drive;
+        public char Drive {
+            get { return _Drive; }
+            set {
+                _Drive = value;
+                RefreshDisplay();
+            }
+        }
+        public void RefreshDisplay() {
+            this.Nodes.Clear();
+            TreeNode RootNode = new TreeNode(_Drive + ":\\");
+            this.Nodes.Add(RootNode);
+            Fill(RootNode);
+            this.Nodes[0].Expand();
+        }
+        private void Fill(TreeNode DirNode) {
+            DirectoryInfo Dir = new DirectoryInfo(DirNode.FullPath);
+            DirectoryInfo DirItem = null;
+            foreach (DirectoryInfo DirItem_loopVariable in Dir.GetDirectories()) {
+                DirItem = DirItem_loopVariable;
+                TreeNode NewNode = new TreeNode(DirItem.Name);
+                DirNode.Nodes.Add(NewNode);
+                NewNode.Nodes.Add("*");
+            }
+        }
+        protected override void OnBeforeExpand(TreeViewCancelEventArgs e) {
+            base.OnBeforeExpand(e);
+            if (e.Node.Nodes[0].Text == "*") {
+                e.Node.Nodes.Clear();
+                Fill(e.Node);
+            }
+        }
+        protected override void OnAfterSelect(TreeViewEventArgs e) {
+            base.OnAfterSelect(e);
+            if (DirectorySelected != null) {
+                DirectorySelected(this, new DirectorySelectedEventArgs(e.Node.FullPath));
+            }
+        }
+    }
+    public class DirectorySelectedEventArgs : EventArgs
+    {
+        public string DirectoryName;
+        public DirectorySelectedEventArgs(string DirectoryName) {
+            this.DirectoryName = DirectoryName;
+        }
+    }
+
+}
---
+++ DDH_UI/MainUi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDH_UI
+{
+    public partial class MainUi : Form
+    {
+        public MainUi() {
+            InitializeComponent();
+
+            DirectoryTree DirTree = new DirectoryTree();
+            DirTree.Size = new Size(this.Width - 30, this.Height - 60);
+            DirTree.Location = new Point(5, 5);
+            DirTree.Drive = 'C';
+            this.Controls.Add(DirTree);
+        }
+    }
+}
